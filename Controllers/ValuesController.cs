cf9c9d9c295216d6247cec726af483a156d8ea97
Gabriel Rigolon Silva <grsilva1@stefanini.com>
2019-12-09 09:26:32 -0300

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProAgil.API.Models;
+
+namespace ProAgil.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Evento>> Get()
+        {
+            return new Evento[] {
+                new Evento()
+                {
+                    EventoId  = 1,
+                    Tema = "Angular e .NET Core",
+                    Local = "Juiz de Fora - MG",
+                    Lote = "1° Lote",
+                    QtdPessoas = Int32.Parse("250"),
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                    
+                }
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
